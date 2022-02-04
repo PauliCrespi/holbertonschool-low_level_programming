83c97e247a36f286f2150639eaaea9c57f8b36da
PauliCrespi <4377@holbertonschool.com>
2022-02-04 10:15:06 -0800

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - print the whole alphabet
+*Return: zero
+*/
+int main(void)
+{
+int comma;
+int ch;
+int cc;
+comma = 44;
+for (ch = '0'; ch <= '9'; ch++)
+{
+for (cc = '0'; cc <= '9'; cc++)
+{
+if (ch != cc)
+{
+putchar (ch);
+putchar (cc);
+if (ch < '9')
+{
+putchar (comma);
+putchar (' ');
+}}
+}}
+putchar ('\n');
+return (0);
+}
