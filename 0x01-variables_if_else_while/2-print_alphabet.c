0111468e91a9b0e647a6a7732df1ba9878e1bd92
PauliCrespi <4377@holbertonschool.com>
2022-02-04 05:53:05 -0800

    task 1 and 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/* more headers goes there */
+/* main - print the whole alphabet
+ * *Return: zero
+ * */
+int main(void)
+{
+for (ch= 'a'; ch<= 'z'; ch++)
+putchar (ch);
+putchar ('\n') ;
+getch();
+return (0);
+}
