a234dcf10085558f0c8a6f42f0591fea645c5682
PauliCrespi <4377@holbertonschool.com>
2022-02-04 10:50:03 -0800

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - print the whole alphabet
+*Return: zero
+*/
+int main(void)
+{
+	int comma;
+	int ch;
+	int cc;
+	int ccc;
+	comma = 44;
+	for (ch = '0'; ch < '9'; ch++)
+	{
+		for (cc = ch; cc <= '9'; cc++)
+		{
+			for (ccc = cc; ccc <= '9'; ccc++)
+			{
+			if (ch != cc && ch != ccc && cc != ccc)
+			{
+				putchar (ch);
+				putchar (cc);
+				putchar (ccc);
+				if (ch < '7' && cc < '9' && ccc <= '9')
+				{
+					putchar (comma);
+					putchar (' ');
+				}}
+		}}}
+	putchar ('\n');
+	return (0);
+}
