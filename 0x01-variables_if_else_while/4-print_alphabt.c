16661e6fa49f10ad907c37c8a49c1bcf807ce6ce
PauliCrespi <4377@holbertonschool.com>
2022-02-04 09:05:29 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,19 @@
-include <stdlib.h>
-/* more headers goes there */
-/* main - print the whole alphabet except q and e
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - print the whole alphabet except q and e
 *Return: zero
 */
 int main(void)
 {
+char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 while (ch != 'q' && ch != 'e')
 {
-putchar (ch'\n');
-getch();
+putchar (ch);
 }
 }
+putchar ('\n');
 return (0);
 }
