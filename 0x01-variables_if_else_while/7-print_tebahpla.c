fccdd95def48268adc66bff0962702ae9ae399e1
PauliCrespi <4377@holbertonschool.com>
2022-02-04 09:33:50 -0800

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 /* more headers goes there */
 /**
- * *main - print the whole alphabet
- * *Return: zero
- * */
+*main - print the whole alphabet
+*Return: zero
+*/
 int main(void)
 {
 char ch;
-for (ch = 'z'; ch <= 'a'; ch--)
+for (ch = 'z'; ch >= 'a'; ch--)
 {
 putchar (ch);
 }
