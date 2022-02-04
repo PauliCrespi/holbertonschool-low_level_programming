014ff41f0ccdb482cedfadcf7c07b5966d1fe7d2
PauliCrespi <4377@holbertonschool.com>
2022-02-04 09:24:09 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - print the whole alphabet
+*Return: zero
+*/
+int main(void)
+{
+int ch;
+for (ch = 0; ch <= 9; ch++)
+{
+putchar (ch);
+}
+putchar ('\n');
+return (0);
+}
