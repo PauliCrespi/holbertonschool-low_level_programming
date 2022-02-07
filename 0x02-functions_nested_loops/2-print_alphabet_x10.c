07d05d9a63bf2f20bc29bce422df642c53ec0bab
PauliCrespi <4377@holbertonschool.com>
2022-02-07 08:16:10 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 #include "1-alphabet.c"
 
 /**
-* print_aplhabet_x10 - check the code.
+*print_aplhabet_x10 - check the code.
 *
 * Return: Always 0.
 */
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 {
 int counter = 0;
 
-while (counter <= 10)
+while (counter < 10)
 {
 print_alphabet();
 counter = (counter + 1);
