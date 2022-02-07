505c138e7e7e8df531370b65a68f950dee7d8059
PauliCrespi <4377@holbertonschool.com>
2022-02-07 12:29:07 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,44 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
 /**
- *print_to_98 - printing
- *@n : num
- *Return: n
- */
+* print_to_98 - computes the absolute value of an integer
+* @n: input creo
+* Return: 0 always
+*/
 void print_to_98(int n)
 {
-int i;
-
-for (i = n; i <= 98; i++)
+if (n <= 98)
 {
-_putchar (i + '0');
-if (i != 98)
-_putchar (',');
-_putchar(' ');
-}}
+	for (n = n; n <= 98; n++)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf("%c", ',');
+			printf("%c", ' ');
+		}
+		else
+		{
+			putchar(10);
+		}
+	}
+}
+else
+{
+	for (n = n; n >= 98; n--)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf("%c", ',');
+			printf("%c", ' ');
+		}
+		else
+		{
+			putchar(10);
+		}
+	}
+}
+}
