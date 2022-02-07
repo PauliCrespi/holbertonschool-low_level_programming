24f9f2799af6539c990db74a5785758d5e1c1fd9
PauliCrespi <4377@holbertonschool.com>
2022-02-07 11:44:12 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,11 @@ int x;
 int multi;
 int v;
 
-for (m = '0'; m <= '9'; m++)
+for (m = 0; m <= 9; m++)
 {
-for (x = '0'; x <= '9'; x++)
+for (x = 0; x <= 9; x++)
 {
-multi = (x * m);
+multi = x * m;
 if (multi > 9)
 {
 v = multi / 10;
