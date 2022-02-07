2d46d3084ef9c06eff83f384066ec737b31dbeb6
PauliCrespi <4377@holbertonschool.com>
2022-02-07 07:10:29 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
 *main - prinf putchar
 *Return: zero
 */
+
 int main(void)
 {
-	_putchar ('_');
-	_putchar ('p');
-	_putchar ('u');
-	_putchar ('t');
-	_putchar ('c');
-	_putchar ('h');
-	_putchar ('a');
-	_putchar ('r');
-	_putchar ('\n');
-	return (0);
+_putchar ('_');
+_putchar ('p');
+_putchar ('u');
+_putchar ('t');
+_putchar ('c');
+_putchar ('h');
+_putchar ('a');
+_putchar ('r');
+_putchar ('\n');
+return (0);
 }
