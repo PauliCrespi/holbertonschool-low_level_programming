60450675787e3f42a4aee42e1db970b6ddd163df
PauliCrespi <4377@holbertonschool.com>
2022-02-07 12:23:04 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_to_98 - printing
+ *@n : num
+ *Return: n
+ */
+void print_to_98(int n)
+{
+int i;
+
+for (i = n; i <= 98; i++)
+{
+_putchar (i + '0');
+if (i != 98)
+_putchar (',');
+_putchar(' ');
+}}
