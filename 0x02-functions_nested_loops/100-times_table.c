e480c23fae6781898ff86ed69269895fd48cd3d6
PauliCrespi <4377@holbertonschool.com>
2022-02-08 18:15:04 -0800

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *print_times_table - n times table
+ *@n : input
+ *Return: n times table
+ */
+int print_times_table(int n)
+{
+{
+int m;
+int x;
+int multi;
+int v;
+
+for (m = 0; m <= n; m++)
+{
+for (x = 0; x <= n; x++)
+{
+multi = x * m;
+if (multi > n)
+{
+v = multi / 10;
+_putchar (v + '0');
+v = multi % 10;
+_putchar(v + '0');
+}
+else
+{
+if (x != 0)
+{
+_putchar (' ');
+}
+_putchar (multi + '0');
+}
+if (x <= (n - 1))
+{
+_putchar (',');
+_putchar (' ');
+}}
+_putchar ('\n');
+}}
