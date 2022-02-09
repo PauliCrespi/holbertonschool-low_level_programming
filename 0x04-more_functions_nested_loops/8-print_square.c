c826c6c573a63f3d625f970fe448b3e4e14d5579
PauliCrespi <4377@holbertonschool.com>
2022-02-09 09:18:48 -0800

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_square - square
+ *@size : size of square input
+ */
+void print_square(int size)
+{
+int lines;
+int hash;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (lines = 0; lines < size; lines++)
+{
+for (hash = 0; hash < size; hash++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
