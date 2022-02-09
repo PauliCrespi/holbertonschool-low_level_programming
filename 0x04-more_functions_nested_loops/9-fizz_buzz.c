9090278a78990a40f48361ef7231cb2ec01f2116
PauliCrespi <4377@holbertonschool.com>
2022-02-09 09:39:43 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *fizzbuzz - fizzingbuzzing
+ */
+void fizzbuzz(void)
+{
+int n;
+
+for (n = 1; n <= 100; n++)
+{
+if ((n % 3) == 0 && (n % 5) == 0)
+{
+printf("FizzBuzz");
+else if ((n % 3) == 0)
+printf("Fizz");
+else if ((n % 5) == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf(n);
+}
+_putchar(' ');
+}
+}
+}
