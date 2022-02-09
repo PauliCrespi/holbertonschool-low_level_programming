6a0c40d59b161248b3103ee4469ea22c39ffda20
PauliCrespi <4377@holbertonschool.com>
2022-02-09 05:55:59 -0800

    tasks

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*more_numbers - print 0 to 9
+*/
+void more_numbers(void)
+{
+int i;
+int n;
+int print;
+
+for (i = 0; i <= 10; i++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n > 9)
+{
+_putchar('1');
+print = n % 10;
+}
+else
+{
+print = n;
+}
+_putchar(print + '0');
+}
+_putchar('\n');
+}
+}
