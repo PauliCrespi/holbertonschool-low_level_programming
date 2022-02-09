46933392bd51730b27e2e109672f7957efe3fd96
PauliCrespi <4377@holbertonschool.com>
2022-02-09 09:54:09 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,13 @@ int n;
 
 for (n = 1; n <= 100; n++)
 {
-if (((n % 3) == 0) && ((n % 5) == 0))
-{
-printf("FizzBuzz");
-}
-else if ((n % 3) == 0)
+if ((n % 3) == 0)
 {
 printf("Fizz");
+if ((n % 5) == 0)
+{
+printf("Buzz");
+}
 }
 else if ((n % 5) == 0)
 {
@@ -23,7 +23,7 @@ printf("Buzz");
 }
 else
 {
-printf(n);
+printf("%d", n);
 }
 _putchar(' ');
 }
