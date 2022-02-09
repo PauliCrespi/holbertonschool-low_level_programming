7a3434e8ccbf491af4e4888c1ad18fadbd8a595c
PauliCrespi <4377@holbertonschool.com>
2022-02-09 11:17:42 -0800

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_triangle - print triangle
+ *
+ *
+ */
+void print_triangle(int size)
+{
+int space;
+int hash;
+int line;
+
+for (line = 1; line <= size; line++)
+{
+for (space = 1; space <= size - line; space++)
+{
+_putchar(' ');
+}
+for (hash = 1; hash <= line; hash++)
+{
+_putchar('#');
+}
+_putchar(10);
+}
+}
