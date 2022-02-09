059f17a098ff826afa95fb8df91a310539c10896
PauliCrespi <4377@holbertonschool.com>
2022-02-09 04:47:34 -0800

    task 0 README and main

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_isupper - check for upper case
+ *@c : int
+ *Return: 1 if upper case, 0 if else
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
