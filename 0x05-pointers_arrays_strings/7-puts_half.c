6083f18a441e8ee51cecba42e6f5780e82c33208
PauliCrespi <4377@holbertonschool.com>
2022-02-11 11:27:25 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *puts_half - puts
+ *@str : s
+ */
+void puts_half(char *str)
+{
+int i;
+int n;
+int m;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+i = i - 1;
+if (i % 2 == 1)
+{
+for (m = 0; s[m] != 0; m++)
+{
+n = (i - 1) / 2;
+if (m == n)
+{
+_putchar(s[m]);
+}
+}
+}
+else
+{
+i = i 2;
+for (m = i; s[m]; m++)
+{
+_putchar(s[m]);
+}
+}
+_putcar('\n');
+}
