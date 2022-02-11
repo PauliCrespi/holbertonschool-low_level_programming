0ff485b781069d098e748ce77373946950bf97be
PauliCrespi <4377@holbertonschool.com>
2022-02-11 12:21:42 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,19 @@ if (n > 0)
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-if (i != (n -1))
+if (i != (n - 1))
 {
 printf(",");
 printf(" ");
 }
 }
 printf("\n");
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+printf(" ");
+}
 }
 }
