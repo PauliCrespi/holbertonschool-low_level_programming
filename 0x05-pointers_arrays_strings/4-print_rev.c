78a4fb2a4141559f4c41223419fb79711b475bb9
PauliCrespi <4377@holbertonschool.com>
2022-02-11 09:46:56 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_rev - print string in reverse
+ *@s : s
+ */
+void print_rev(char *s)
+{
+int m;
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+
+for (m = i; m >= 0; m--)
+{
+_putchar(s[m]);
+}
+_putchar('\n');
+}
