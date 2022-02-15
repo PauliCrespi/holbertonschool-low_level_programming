55171e9c3622c8b9222d193783cf3983e5269a6a
PauliCrespi <4377@holbertonschool.com>
2022-02-15 09:37:40 -0800

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strcmp - str
+ *@s1 : s
+ *@s2 : s2
+ *Return: counter
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int j;
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+
+}
+if (i == j)
+{
+return (0);
+}
+else if (i < j)
+{
+return (-15);
+}
+return (15);
+}
