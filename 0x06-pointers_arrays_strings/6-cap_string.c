fa122fcf997afb01662cfb33d9d2c499584261e9
PauliCrespi <4377@holbertonschool.com>
2022-02-15 11:24:53 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *cap_string - changing
+ *@arrstr : char
+ *Return: arrstr
+ */
+char *cap_string(char *arrstr)
+{
+	int i;
+	int j;
+	char flags1[] = {'\n', '\t', ',', ';', '.', '!', '?', ')', '}', '(', '{', '"', ' '};
+
+	for (i = 0; arrstr[i] != '\0'; i++)
+	{
+		for (j = 0; flags1[j] != '\0'; j++)
+		{
+			if (arrstr[i] == flags1[j])
+			{
+				if (arrstr[i + 1] == ' ')
+				{
+					if (arrstr[i + 2] >= 'a' && arrstr[i] <= 'z')
+					{
+						arrstr[i + 2] = arrstr[i + 2] - 32;
+					}
+				}
+				else if (arrstr[i + 1] >= 'a' && arrstr[i + 1] <= 'z')
+				{
+					arrstr[i + 1] = arrstr[i + 1] -32;
+				}
+			}
+		}
+	}
+	return (arrstr);
+}
