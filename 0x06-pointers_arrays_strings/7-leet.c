5c6839667f25333b9dc7d086258516284c54ed76
PauliCrespi <4377@holbertonschool.com>
2022-02-16 08:33:39 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *leet - array
+ *@arr : array
+ *Return: char
+ */
+char *leet(char *arr)
+{
+	int i;
+	int j;
+	char lower[] = {'a', 'e', 'o', 't', 'l'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
+	int nums[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; arr[i] != '\0'; i++)
+	{
+		for (j = 0; lower[j] != '\0'; j++)
+		{
+		if (arr[i] == lower[j] || arr[i] == upper[j])
+		{
+			arr[i] = nums[j];
+		}
+		}
+	}
+return (arr);
+}
