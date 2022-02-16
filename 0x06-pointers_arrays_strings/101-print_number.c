efced5cf634cf6c1eb2518365adbbbadd95b648e
PauliCrespi <4377@holbertonschool.com>
2022-02-16 09:32:37 -0800

    task 101 and main

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_number - print int
+ *@n : input
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,3 +19,7 @@ char *cap_string(char *);
 char *leet(char *);
 
 char *rot13(char *);
+
+void print_number(int n);
+
+int _putchar(char c);
