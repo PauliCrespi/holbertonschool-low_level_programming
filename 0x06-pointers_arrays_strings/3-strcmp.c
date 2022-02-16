3059fae5c925590b5aed558f7edec25b42752090
PauliCrespi <4377@holbertonschool.com>
2022-02-16 05:53:17 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,32 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-int ret = 0;
+	int i;
+	int ret = 0;
 
-for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-{
-if (s1[i] == s2[i])
-{
-ret = ret + 0;
-}
-else if (s1[i] < s2[i])
-{
-ret = ret + (s2[i] - s1[i]);
-}
-ret = ret + (s1[i] - s2[i]);
-}
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			return (0);
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	i--;
+	if (s1[i] != '\0' && s2[i] != '\0')
+	{
+		return (0);
+	}
+	else if (s1[i] != '\0' && s2[i] == '\0')
+	{
+			return (s1[i]);
+	}
+	else if (s1[i] == '\0' && s2[i] != '\0')
+	{
+			return (s2[i]);
+	}
 return (ret);
 }
