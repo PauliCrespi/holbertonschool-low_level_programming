47c737c759f3dfa621e88e18cf880ab613a6aae2
PauliCrespi <4377@holbertonschool.com>
2022-02-21 11:19:38 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,36 +7,42 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int j;
-	char *c;
+	int i = 0;
 	int flag = 0;
+	int j;
+	int f;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (haystack[i])
 	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i] == needle[j])
-			{
-				flag = 1;
-				while (haystack[i] != ' ' && haystack[i] != '\0')
-				{
-					i++;
-					j++;
-					if (haystack[i] != needle[j])
-					flag = 0;
-					c = &haystack[i - 1];
-					break;
-				}
-			}
-			if (flag == 1)
-				break;
-		}
-		if (flag == 1)
-			break;
+		if (haystack[i] == needle[0])
+			flag = 1;
+		i++;
 	}
 	if (flag == 0)
+	{
 		return (0);
-	else
-		return (c);
-}
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			f = i;
+			for (j = 0; needle[j] != '\0'; j++, f++)
+			{
+				if (haystack[f] == needle[j])
+					flag = 2;
+				else
+					break;
+				if (flag != 2)
+					break;
+			}
+			if (flag != 2)
+				break;
+		}
+		if (flag == 2)
+			break;
+		else
+			haystack++;
+	}
+	return (haystack);
+}		
