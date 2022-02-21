9deddea50eaad3413fe79fa16dbbefe7468634f2
PauliCrespi <4377@holbertonschool.com>
2022-02-21 06:14:00 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memcpy - memory copy
+ *@dest : pointer
+ *@src : src pointer
+ *@n : intput unsigned int
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*src = *dest;
+	}
+	return (dest);
+}
