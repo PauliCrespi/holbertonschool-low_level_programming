ae28948095b80ef43528bee7a2cea26338c8b91d
PauliCrespi <4377@holbertonschool.com>
2022-02-23 04:46:46 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts_recursion - print a string
+ *@s : pointer
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != 0)
+	{
+		_puts_recursion(s);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
