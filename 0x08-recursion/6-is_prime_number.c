e0d001ea8188df30eac8b843db4fa6d345f4b426
PauliCrespi <4377@holbertonschool.com>
2022-02-23 11:17:46 -0800

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * primenums - prime numbers
+ * @n : int
+ * @i : int
+ * Return: int
+ */
+int primenums(int n, int i)
+{
+	if (i > n / 2)
+		return (1);
+	if (n % i == 0)
+		return (0);
+		return (primenums(n, i + 1));
+}
+/**
+ * is_prime_number - main
+ * @n : int
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n < 1)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (primenums(n, 2));
+}
