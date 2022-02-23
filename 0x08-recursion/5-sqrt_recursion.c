c0853e63341e013e9adbcf952771b67ef11b1f57
PauliCrespi <4377@holbertonschool.com>
2022-02-23 11:05:38 -0800

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *square - natural square root
+ *@n : int
+ *Return: int
+ */
+int square(int n, int i)
+{
+	if (i * i == n)
+	{
+		return(i);
+	}
+	else if (i == n)
+	{
+		return (-1);
+	}
+	else if (i * i != n)
+	{
+		return (square(n, i + 1));
+	}
+	return (0);
+}
+/**
+ *_sqrt_recursion - square root
+ *@n : int
+ *Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	int root;
+
+	if (n < 0)
+	{
+		root = -1;
+	}
+	else
+	{
+	root = square(n, 1);		
+	}
+	return (root);
+}
