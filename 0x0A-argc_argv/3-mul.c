8e1f43bb0c328d5c26e736395d6105c24cf76c39
PauliCrespi <4377@holbertonschool.com>
2022-02-25 07:48:49 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i = 1;
+	int one, two;
 
 	if (argc == 3)
 	{
-		i = atoi(argv[1]) * atoi(argv[2]);
+		one = atoi(argv[1]);
+		two = atoi(argv[2]);
+		i = one * two;
 		printf("%d\n", i);
 	}
 	if (argc != 3)
 	{
 		printf("Error\n");
-		i = 1;
 	}
 	return (i);
 }
