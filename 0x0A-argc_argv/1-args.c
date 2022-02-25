e02e19c84b373868ec11c0cb8a75ef2ec15a07fa
PauliCrespi <4377@holbertonschool.com>
2022-02-25 07:15:26 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-int main(int argc, __attribute__((unused)) char *argv[]) 
+/**
+ *main - main
+ *@argc : argc
+ *@argv : pointer
+ *Return: i
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	int i = -1;
 
-	while (i < argc - 1) 
+	while (i < argc - 1)
 	{
 		i++;
-	}	
+	}
 	printf("%d\n", i);
 	return (i);
 }
