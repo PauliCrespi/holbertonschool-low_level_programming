d176dc7523a20b1684d9b27dfcf4a1bbbffd226c
PauliCrespi <4377@holbertonschool.com>
2022-02-25 07:13:08 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ *main - main function
+ *@argc : unused
+ *@argv : pointer
+ *Return: 0
+ */
 int main(__attribute__((unused))int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
