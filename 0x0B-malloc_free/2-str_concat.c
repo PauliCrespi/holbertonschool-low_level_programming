2150cfa9b5e6d5b0ecd97b6519b959c8afc92b73
PauliCrespi <4377@holbertonschool.com>
2022-03-02 09:14:20 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,16 +24,16 @@ char *str_concat(char *s1, char *s2)
 	{
 
 	}
-	sumsize = size + sizee + 2;
+	sumsize = size + sizee + 1;
 	p = malloc(sumsize * sizeof(char));
-	if (size != 0 || s1 != NULL)
+	if (size != 0 && s1 != NULL)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 		{
 			p[i] = s1[i];
 		}
 	}
-	if (sizee != 0 || s2 != NULL)
+	if (sizee != 0 && s2 != NULL)
 	{
 		for (j = 0; s2[j] != '\0'; j++, i++)
 		{
