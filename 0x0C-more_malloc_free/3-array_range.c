89017b7ced3ae3f5171dcda8d18249a9a2c12b5e
PauliCrespi <4377@holbertonschool.com>
2022-03-03 16:42:33 -0800

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,30 +14,29 @@ int *array_range(int min, int max)
 	int j;
 	int mem = 0;
 
-	if (min > max)
+	if (min <= max)
 	{
-		return (0);
-	}
-	else if (min == max)
-	{
-		p = malloc((sizeof(min) * sizeof(int)) + (sizeof(max) * sizeof(int)));
-		if (p == NULL)
-			return (0);
-		p[0] = min;
-		p[1] = max;
-	}
-	else
-	{
-		p = malloc(0);
-		if (p == NULL)
-			return (0);
-		for (j = min; j <= max; j++, i++)
+		if (min == max)
 		{
-			mem = mem + (sizeof(j) * sizeof(int));
-			p = (int *) realloc(p, mem);
+			p = malloc((sizeof(min) * sizeof(int)) + (sizeof(max) * sizeof(int)));
 			if (p == NULL)
 				return (0);
-			p[i] = j;
+			p[0] = min;
+			p[1] = max;
+		}
+		else
+		{
+			p = malloc(0);
+			if (p == NULL)
+				return (0);
+			for (j = min; j <= max; j++, i++)
+			{
+				mem = mem + (sizeof(j) * sizeof(int));
+				p = (int *) realloc(p, mem);
+				if (p == NULL)
+					return (0);
+				p[i] = j;
+			}
 		}
 	}
 	return (p);
