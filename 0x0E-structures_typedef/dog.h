c1d7e7664a045c7876d410307817a3598672d43a
PauliCrespi <4377@holbertonschool.com>
2022-03-08 07:09:50 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
+#ifndef DOG
+#define DOG
+
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
