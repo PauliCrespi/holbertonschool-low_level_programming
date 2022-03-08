64877230d3155b2baaee74ea4f87a3ceb395d34f
PauliCrespi <4377@holbertonschool.com>
2022-03-08 07:00:51 -0800

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog - function
+ *@d : pointer
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
