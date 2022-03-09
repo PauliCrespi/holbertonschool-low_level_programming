df4c57aa5db916b0f651f774340b9fd994416115
PauliCrespi <4377@holbertonschool.com>
2022-03-09 07:25:47 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ *print_name - function
+ *@name : pointer
+ *@f : pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	{
+		f(name);
+	}
+}
