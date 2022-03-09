1313ed798d68e44e6683704d12b2725ff98161c0
PauliCrespi <4377@holbertonschool.com>
2022-03-09 07:45:34 -0800

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *int_index - function
+ *@array : pointer
+ *@size : int
+ *@cmp : pointer to function
+ *Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size && !cmp(array[i]); i++)
+		{
+			cmp(array[i]);
+		}
+		if (cmp == 0)
+			return (-1);
+	}
+	return (i);
+}
