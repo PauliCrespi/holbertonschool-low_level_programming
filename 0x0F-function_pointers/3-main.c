e90c888413d974512dc492d621e7e2b9a77591e1
PauliCrespi <4377@holbertonschool.com>
2022-03-09 14:16:04 -0800

    main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function
+ *@argc : argc
+ *@argv : argv
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(a, b));
+	return (0);
+}
