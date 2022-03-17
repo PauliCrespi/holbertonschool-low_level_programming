2c58692ba6ccc156635367ce11f8259d322c9679
PauliCrespi <4377@holbertonschool.com>
2022-03-17 12:25:17 -0700

    task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,4 @@ void print_bf_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
 }
