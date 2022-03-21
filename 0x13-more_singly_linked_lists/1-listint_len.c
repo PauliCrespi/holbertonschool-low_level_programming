84e3fc1ee9f0af0d904c9e39ac56a9f74ca0b228
PauliCrespi <4377@holbertonschool.com>
2022-03-21 06:58:41 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *listint_len : length of list
+ *@h : pointer
+ *Return: length
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
