d91bcd9120a0314368b41be888905e7968d2d89e
PauliCrespi <4377@holbertonschool.com>
2022-03-22 17:19:00 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		check = check->next;
 		i++;
 	}
-	if (i == (idx - 1))
+	if (i == (idx - 1) && check != NULL)
 	{
 		newnode->next = check->next;
 		check->next = newnode;
