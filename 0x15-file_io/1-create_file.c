65645822abd75280cda62fadd4a4bee2e8f162b2
PauliCrespi <4377@holbertonschool.com>
2022-03-28 12:57:17 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,9 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
-	if (text_content[0] ==  '\0')
+	if (!text_content)
 		return (1);
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 	{
 	}
 	fw = write(fp, text_content, i);
