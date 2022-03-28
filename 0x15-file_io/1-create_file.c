7b40116027e5cafd68dab87d37372c2d6173f2e1
PauliCrespi <4377@holbertonschool.com>
2022-03-28 12:40:34 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *create_file - func
+ *@filename : pointer of name
+ *@text_content : pointer to content
+ *Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, fp, fw;
+
+	if (filename == NULL)
+		return (-1);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1);
+	for (i = 0; i != '\0'; i++)
+	{
+	}
+	fw = write(fp, text_content, i);
+	if (fw == -1)
+		return (0);
+	close(fp);
+	return (1);
+}
