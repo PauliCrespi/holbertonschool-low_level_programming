70a037f50ef996ba9fbb5e24ce35e03e04560f3b
PauliCrespi <4377@holbertonschool.com>
2022-05-05 06:52:30 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	size_t i = 0;
+
+	while (node != NULL)
+	{
+		i++;
+		node = node->next;
+	}
+	return (i);
+}
