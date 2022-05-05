a03a9b82cb2f72c3c67aec3a8ff82866beb48338
PauliCrespi <4377@holbertonschool.com>
2022-05-05 09:01:30 -0700

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,11 +12,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (newnode == NULL)
 		return (NULL);
-	if (!n)
-	{
-		free(newnode);
-		return (NULL);
-	}
 	newnode->n = n;
 	newnode->next = *head;
 	newnode->prev = NULL;
