db993b55d9832a8173bbae509400a50a2417acfb
PauliCrespi <4377@holbertonschool.com>
2022-05-05 11:17:49 -0700

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ *delete_dnodeint_at_index - main
+ *@head: list.
+ *@index : int
+ *Return: int
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *check = *head;
+	dlistint_t *aux = *head;
+	unsigned int i = 0;
+
+	if (check == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = aux->next;
+		free(aux);
+		if (*head)
+			(*head)->prev = NULL;
+		return (1);
+	}
+	index--;
+	while (check && i != index)
+	{
+		check = check->next;
+		i++;
+	}
+	if (i == index && check)
+	{
+		aux = check->next;
+		if(aux->next)
+			aux->next->prev = check;
+		check->next = aux->next;
+		free(aux);
+		return (1);
+	}
+
+	return (-1);
+}
