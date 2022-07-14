dacf522646e0ecff5d5ad644c65efb2769bc75d6
PauliCrespi <4377@holberton.com>
2022-07-14 08:10:56 -0700

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,7 +3,7 @@
 def island_perimeter(grid):
     """grid island is a list"""
     per = 0
-    for i in range(len(grid)):
+    for i in range(1, len(grid) - 1):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 per = per + 4
